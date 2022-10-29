6c9a99243a1d05df94f39109b97cda221fa0dd01
putrid#0196 <43300931+PutridRapesSkids@users.noreply.github.com>
2022-10-29 15:11:52 -0400

    Create type.js

---
+++ type.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+    strings : ['xeno','banner god','scripter', 'best vault owner', 'top known' '.gg/unlegit', '.gg/prediction'],
+    typeSpeed : 40,
+    delaySpeed : 90,
+    showCursor: false,
+    loop : true
+  });
