82a2551fcb47278132c7330ea8164db08a2b0a09
putrid#0196 <43300931+PutridRapesSkids@users.noreply.github.com>
2022-10-29 15:12:49 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("putrid#0196 | xeno#0666");
+
+function audioPlay() {
+  var audio = document.getElementById("audio");
+  audio.volume = 0.3;
+  audio.play()
+}
+
+function videoPlay() {
+  var video = document.getElementById("video");
+  video.play()
+}
